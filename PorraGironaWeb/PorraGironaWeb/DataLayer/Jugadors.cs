91a0377a3233a90312bc0f032c59f40f96a84083
Pozok05 <a2308766@institutmontilivi.cat>
2024-05-10 11:47:18 +0200

    Start

---
+++ PorraGironaWeb/PorraGironaWeb/DataLayer/Jugadors.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PorraGironaWeb.DataLayer;
+
+public partial class jugadors
+{
+    public int idjugador { get; set; }
+
+    public string? nom { get; set; }
+
+    public int? idequip { get; set; }
+
+    public string? temporada { get; set; }
+
+    public virtual equips? idequipNavigation { get; set; }
+}
---
+++ PorraGironaWeb/PorraGironaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PorraGironaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
